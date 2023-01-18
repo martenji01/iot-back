a5a0627218c90ab6a0b92e32d23294dc089191b1
andreacastaa <95278978+andreacastaa@users.noreply.github.com>
2023-01-18 16:26:10 +0100

    add API-GET-DATA

--- Jac-Aws-IoT-main/index.js
+++ Jac-Aws-IoT-main/index.js
@@ -1,8 +1,8 @@
 import { checkMasterCertificate } from "./iot/certManager.js";
 import { initDevice } from "./iot/index.js";
 import {databaseconnect} from "./mongoose/database.js"
-
 import express from "express";
+import Datalogs from './mongoose/schema.js'
 
 const app = express();
 
@@ -17,6 +17,20 @@ await initDevice();
 
 await databaseconnect ()
 
+app.use(express.json());
+
+// api GET  data
+app.get("/data", async (req, res) => {
+  const allData = await Datalogs.find();
+  return res.status(200).json(allData);
+});
+
 app.listen(9000, () => {
     console.log('Server listening on port 9000');
+
 });
+
+
+
+
+
