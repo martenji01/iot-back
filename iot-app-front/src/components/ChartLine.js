7f04251883b225ea1ebe156209fa651ee29d2aaf
martenji01 <martacrd01@gmail.com>
2023-01-23 12:13:08 +0100

    feat(chart):timestamp

--- iot-app-front/src/components/ChartLine.js
+++ iot-app-front/src/components/ChartLine.js
@@ -32,7 +32,24 @@ ChartJS.register(
   
 const ChartLine = ({list, title, property}) => {
     const [datasetList, setDatasetList] = useState([])
-    const labels = list ? list.map((log)=>log[datalog_properties.timestamp]) : [];
+    const convertTimestamp = (unix_timestamp)=>{
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+        console.log(formattedTime);
+        return formattedTime
+      }
+    const labels = list ? list.map((log)=>convertTimestamp(log[datalog_properties.timestamp])) : [];
     const data = {
         labels,
         datasets: datasetList
@@ -50,7 +67,7 @@ const ChartLine = ({list, title, property}) => {
         },
       };
       
-
+      
       const loadDataset= ()=>{
         const listData = property.map((p)=>{
             return {
